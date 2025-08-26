d6833037d018a0f2d4d171937fef69eaace93b5e
Jalen <Jaytw1502@gmail.com>
2025-08-26 15:00:54 -0500

    add gitignore to stop venv errors

--- data-generators/sensor_simulator.py
+++ data-generators/sensor_simulator.py
@@ -1,6 +1,8 @@
 import json
 import random
-from confluent_kafka import Producer
+from confluent_kafka import Producer, Consumer, KafkaError
+import time
+
 locationName = {"TEMP_COLD_AISLE_A01":{'min': 64.4, 'max': 68 }, 
                 "TEMP_HOT_AISLE_A01":{'min': 77.0, 'max': 86.0}, 
                 "TEMP_CRAC_UNIT_01":{'min': 61.0, 'max': 64.0},
@@ -10,10 +12,12 @@ locationName = {"TEMP_COLD_AISLE_A01":{'min': 64.4, 'max': 68 },
 
 def getData():
     result = []
+    result.append({"timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+                   "sensor_type": "temperature"})
     for key, value in locationName.items():
         temp = random.uniform(value['min'], value['max'])
-        result.append({"location": key,
-                    "temperature": round(temp, 2),
+        result.append({"device_id": key,
+                    "value": round(temp, 2),
                     "unit": "fahrenheit"})
     return json.dumps(result, indent=2)
 
@@ -23,4 +27,36 @@ def streamData():
     producer.produce('temp_recorded', value=res.encode('utf-8'))
     producer.flush()
 
-streamData()
+def start_consumer(bootstrap_server = 'localhost:9092', group_id ='temp_comsumer'):
+    config = {
+        'bootstrap.servers': bootstrap_server, #for now this is local connection, later once we containerize the code we switch to 9093
+        'group.id': group_id,
+        'auto.offset.reset': 'earliest',
+        'enable.auto.commit': True
+    }
+    consumer = Consumer(config)
+    consumer.subscribe(["temp_recorded"])
+    return consumer
+
+def consumeData(consumer):
+    message = consumer.poll(timeout=0.1)
+    if message is None:
+        return  # This prevents the crash
+        
+    if message.error():
+        if message.error().code() == KafkaError._PARTITION_EOF:
+            return
+        else:
+            print(f"Error: {message.error()}")
+            return
+    raw_value = message.value().decode('utf-8')
+    print(raw_value)
+    
+
+
+consumer = start_consumer()
+
+while True:
+    streamData()
+    time.sleep(2)
+    consumeData(consumer)
